2a5b1d90a8cda11378328c473f841af919a87e54
lyp830414 <lyp830414@163.com>
2018-10-07 05:11:00 -0700

    add mongodb-server and mongodb installation part

--- test.sh
+++ test.sh
@@ -31,25 +31,28 @@ function ssldepends()
     cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
     make
     make install
+    
+    sleep 3
 
     #./configure --disable-automatic-init-and-cleanup --enable-static
     #make
     #make install
     #cd ..
  
-    echo "LYP 2--1" 
     wget https://github.com/mongodb/mongo-cxx-driver/archive/r3.2.0.tar.gz --directory-prefix=/home/bottos
-    echo "LYP 2--2: $USER_HOME_DIR" 
     if [ ! -f /home/bottos/r3.2.0.tar.gz ];
     then
         echo "*ERROR* Fail to find the tar file: r3.2.0.tar.gz !"
         exit 1
     fi
-    echo "LYP 2--3" 
 
-    #exit 1
-    
-    tar -xzvf /home/bottos/r3.2.0.tar.gz
+    tar xzf /home/bottos/r3.2.0.tar.gz
+    if [ ! -d /home/bottos/mongo-cxx-driver-r3.2.0 ];
+    then
+         echo "*ERROR* Fail to find the tar file: mongo-cxx-driver-r3.2.0 !"
+ 	 exit 1
+    fi
+
     cd /home/bottos/mongo-cxx-driver-r3.2.0/build
     cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
     make EP_mnmlstc_core
@@ -61,4 +64,5 @@ function ssldepends()
     #rm -rf $USER_HOME_DIR/r3.2.0*
 }
 
+apt-get install mongodb-server mongodb -y
 ssldepends
